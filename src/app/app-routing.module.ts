4c044307579e08a6740a1dab244d4cb74d925403
PaulaPineros <pineros.paula@javeriana.edu.co>
2021-09-04 11:25:37 -0500

    Registro y logeo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from "./admin/admin.component";
 import { CatalogoComponent } from './catalogo/catalogo.component';
+import { RegistroComponent } from './registro/registro.component';
+import { SesionComponent } from './sesion/sesion.component';
 
 const Routes = [
   {path: 'admin', canActivate:[], component: AdminComponent},
   {path: 'catalogo', component: CatalogoComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'sesion', component: SesionComponent},
 ];
 
 
